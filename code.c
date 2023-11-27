82dac3912a41e1bdb0be68e0b5f08c261e023cc9
Darlene Jiang <darlenej@rflambda.com>
2023-11-27 10:33:36 -0800

    updated by danlin

--- code.c
+++ code.c
@@ -5,5 +5,5 @@ int main (){
     printf("Complted list: \ndarlene\n");
     //TODO: Add your code after this line
     //example: printf("[NAME]\n");
-    printf("danlin\n");
+    printf("danlin \n");
 }
